ea29084c8babbe0fb4c6c7e9c865eecd7938580b
Alex Kaforey <akafore1@binghamton.edu>
2023-01-27 15:08:09 -0500

    add timer, vectors to allow greater num_samples

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <pthread.h>
 #include <iostream>
+#include <iomanip>
 #include <cmath>
+#include <vector>
+#include <chrono>
 
 using namespace std;
 
@@ -13,7 +16,6 @@ output:
     result, time taken
 */
 
-// TODO Take command-line input
 // TODO Time calculation
 // TODO Optimize for speed
 
@@ -24,8 +26,8 @@ struct parameters{
     int T; // Number of threads
     pthread_mutex_t mutex; // Mutex to protect the shared result variable
     // Gaussian quadrature weights and points
-    double* x;
-    double* w;
+    vector<double> x;
+    vector<double> w;
     double* result;
 };
 
@@ -35,6 +37,35 @@ double f(double x) {
     return sin(x)/x;
 }
 
+// double average(const std::vector<double>& values) {
+//     double sum = 0.0;
+//     double error = 0.0;
+//     for (double value : values) {
+//         double y = value - error;
+//         double temp = sum + y;
+//         error = (temp - sum) - y;
+//         sum = temp;
+//     }
+//     return sum / values.size();
+// }
+
+// double pairwiseSum(const std::vector<double>& values) {
+//     // Recursively add pairs of numbers together
+//     if (values.size() == 1) {
+//         return values[0];
+//     }
+//     if (values.size() == 2) {
+//         return values[0] + values[1];
+//     }
+//     std::vector<double> newValues;
+//     for (int i = 0; i < values.size() - 1; i += 2) {
+//         newValues.push_back(values[i] + values[i + 1]);
+//     }
+//     if (values.size() % 2 != 0) {
+//         newValues.push_back(values.back());
+//     }
+//     return pairwiseSum(newValues);
+// }
 
 void* integrate(void* params) {
     struct parameters* vars = (struct parameters*) params;
@@ -43,9 +74,10 @@ void* integrate(void* params) {
     // Calculate the integral for this thread
     for (long i = vars->thread_id; i < vars->N; i += vars->T) {
         thread_result += vars->w[i] * f(vars->x[i]);
+        // vars->result[i] = vars->w[i] * f(vars->x[i]);
     }
 
-    // Add this thread's result to the global result
+    // // Add this thread's result to the global result
     pthread_mutex_lock(&(vars->mutex));
     *(vars->result) += thread_result;
     pthread_mutex_unlock(&(vars->mutex));
@@ -55,12 +87,14 @@ void* integrate(void* params) {
 
 int main(int num_args, char** args) {
 
+
     if (num_args != 5){
         cout << "Wrong number of arguments" << endl;
         cout << num_args << endl;
         return 0;
     }
 
+    auto start = std::chrono::high_resolution_clock::now();
 
     // Set the integral limits
     double a = stod(args[1]);
@@ -68,9 +102,15 @@ int main(int num_args, char** args) {
     int num_samples = stoi(args[3]);
     int num_threads = stoi(args[4]);
 
-    double result = 0;     // Result of the integral calculation
-    double x[num_samples];
-    double w[num_samples];
+    // vector<double> result;
+    // result.resize(num_samples);
+    double result;     // Result of the integral calculation
+    // double x[num_samples];
+    vector<double> x;
+    x.resize(num_samples);
+    // double w[num_samples];
+    vector<double> w;
+    w.resize(num_samples);
 
     // Initialize the mutex
     pthread_mutex_t mutex; // Mutex to protect the shared result variable
@@ -97,10 +137,16 @@ int main(int num_args, char** args) {
         pthread_join(threads[i], NULL);
     }
 
-    // Print the result
-    cout << "Result: " << result << endl;
+    // double final_result = pairwiseSum(result)/num_samples;
 
-    // Destroy the mutex
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+
+    std::cout << "Duration: " << duration.count() << " milliseconds" << std::endl;
+
+    // Print the result
+    cout << "Result: " << fixed << setprecision(18) << result << endl;
+
     pthread_mutex_destroy(&mutex);
 
     return 0;
